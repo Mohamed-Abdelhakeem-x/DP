31f51cc1d7e5f62268ae6b0ae7f7727d0dca7dc8
Mohamed Abdelhakeem <mohamed.abdelhakeemx@gmail.com>
2025-11-12 06:33:40 +0200

    Create F- Grid 1.cpp

---
+++ Recursive (Vjudge)/F- Grid 1.cpp
@@ -0,0 +1,106 @@
+/*
+Author: WolfHakeem
+*/
+
+#include <bits/stdc++.h>
+//#include "cp-algo/number_theory/modint.hpp"
+//#include "cp-algo/math/combinatorics.hpp"
+using namespace std;
+#define ll long long
+#define u128 unsigned __int128
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vll vector<ll>
+#define vvll vector<vector<ll>>
+#define vb vector<bool>
+#define vc vector<char>
+#define vs vector<string>
+#define vvc vector<vector<char>>
+#define vvb vector<vector<bool>>
+#define vd vector<double>
+#define vpii vector<pair<int,int>>
+#define vpll vector<pair<ll,ll>>
+#define readv(v) for(auto& i : v) cin >> i
+#define printv(v) for(auto& i : v) cout << i << " "
+#define all(x) (x).begin(), (x).end()
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define read128(x) { \
+string s; cin >> s; \
+__int128 val = 0; bool neg = false; \
+if (s[0] == '-') { neg = true; s = s.substr(1); } \
+for (char c : s) val = val * 10 + (c - '0'); \
+(x) = neg ? -val : val; \
+}
+#define print128(x) { \
+__int128 val = (x); \
+if (val == 0) { cout << "0"; } \
+else { \
+if (val < 0) { cout << "-"; val = -val; } \
+string s; \
+while (val > 0) { s += '0' + (val % 10); val /= 10; } \
+reverse(s.begin(), s.end()); \
+cout << s; \
+} \
+}
+//--------------------------------------------------Global Memory-------------------------------------------------------
+int h,w;
+vvc grid;
+ll const N = 1001;
+const ll MOD = 1e9+7;
+ll dp[N][N];
+//----------------------------------------------------Functions---------------------------------------------------------
+void fast() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void set_file(string &file_name) {
+    freopen((file_name + ".in").c_str(), "r", stdin);
+    freopen((file_name + ".out").c_str(), "w", stdout);
+}
+
+ll spell(int i, int j) {
+    if (i == h-1 && j == w-1) return 1;
+    
+    if (i >=h || j >= w || grid[i][j] == '#') return 0;
+
+    if (~dp[i][j])
+        return dp[i][j];
+
+    ll ch1 = spell(i+1, j);
+    ll ch2 = spell(i, j+1);
+
+    return dp[i][j] = (ch1 + ch2) % MOD;
+}
+
+void magic() {
+    cin >> h >> w;
+
+    grid.resize(h, vc(w));
+    for (int i=0; i<h; i++)
+        for (int j=0; j<w; j++)
+            cin >> grid[i][j];
+
+    memset(dp, -1, sizeof(dp));
+
+    cout << spell(0, 0);
+}
+//------------------------------------------------------Main------------------------------------------------------------
+signed main() {
+    fast();
+
+    int t;
+    t=1;
+
+    while (t--) {
+        magic();
+    }
+
+    return 0;
+}
